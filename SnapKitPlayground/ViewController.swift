07ac806ae3bd53d28fc6919743dab148552cb2d8
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2017-02-20 00:43:29 -0300

    Initial Commit

---
+++ SnapKitPlayground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapKitPlayground
+//
+//  Created by Rafael Ribeiro Correia on 2/20/17.
+//  Copyright © 2017 Rafael Ribeiro Correia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
